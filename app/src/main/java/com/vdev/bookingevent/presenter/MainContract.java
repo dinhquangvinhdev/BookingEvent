9b17648182edfbe09dff34e98f243b96bd331a1e
dinhquangvinhdev <121219528+dinhquangvinhdev@users.noreply.github.com>
2023-04-03 19:35:49 +0700

    delete view in mainView

--- app/src/main/java/com/vdev/bookingevent/presenter/MainContract.java
+++ app/src/main/java/com/vdev/bookingevent/presenter/MainContract.java
@@ -4,12 +4,10 @@ import android.net.Uri;
 
 public interface MainContract {
     interface View{
-        void startActivity(Class classActivity);
-        void showUserInfor(String email, String name , Uri avatar);
+
     }
 
     interface Presenter{
-        void logout();
-        void checkUser();
+
     }
 }
