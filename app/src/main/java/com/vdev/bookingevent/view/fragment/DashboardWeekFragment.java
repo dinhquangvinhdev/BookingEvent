130b2b39bb524e9a63b7d7c1f8e7cb5be3460e70
dinhquangvinhdev <121219528+dinhquangvinhdev@users.noreply.github.com>
2023-04-05 15:36:49 +0700

    add library for week view

--- app/src/main/java/com/vdev/bookingevent/view/fragment/DashboardWeekFragment.java
+++ app/src/main/java/com/vdev/bookingevent/view/fragment/DashboardWeekFragment.java
@@ -2,15 +2,24 @@ package com.vdev.bookingevent.view.fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.alamkanak.weekview.MonthLoader;
+import com.alamkanak.weekview.WeekViewEvent;
 import com.vdev.bookingevent.R;
+import com.vdev.bookingevent.databinding.FragmentDashboardWeekBinding;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DashboardWeekFragment extends Fragment {
+    private FragmentDashboardWeekBinding binding;
 
     public DashboardWeekFragment() {
         // Required empty public constructor
@@ -25,6 +34,25 @@ public class DashboardWeekFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dashboard_week, container, false);
+        binding = FragmentDashboardWeekBinding.inflate(inflater, container ,false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        //create calendar
+        initCalendar();
+    }
+
+    private void initCalendar() {
+        binding.weekView.setMonthChangeListener(new MonthLoader.MonthChangeListener() {
+            @Override
+            public List<? extends WeekViewEvent> onMonthChange(int newYear, int newMonth) {
+                List<WeekViewEvent> events = new ArrayList<>();
+                return events;
+            }
+        });
     }
 }
