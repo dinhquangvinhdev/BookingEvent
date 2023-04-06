e055476feff88d16cd3c483826159591cf016fdb
dinhquangvinhdev <121219528+dinhquangvinhdev@users.noreply.github.com>
2023-04-06 20:00:28 +0700

    add temp event in to calendar

--- app/src/main/java/com/vdev/bookingevent/view/fragment/DashboardWeekFragment.java
+++ app/src/main/java/com/vdev/bookingevent/view/fragment/DashboardWeekFragment.java
@@ -1,22 +1,32 @@
 package com.vdev.bookingevent.view.fragment;
 
+import static android.content.ContentValues.TAG;
+
+import android.graphics.Color;
+import android.graphics.RectF;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.alamkanak.weekview.MonthLoader;
+import com.alamkanak.weekview.WeekView;
 import com.alamkanak.weekview.WeekViewEvent;
 import com.vdev.bookingevent.R;
 import com.vdev.bookingevent.databinding.FragmentDashboardWeekBinding;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
+
+import javax.xml.namespace.QName;
 
 public class DashboardWeekFragment extends Fragment {
     private FragmentDashboardWeekBinding binding;
@@ -34,7 +44,7 @@ public class DashboardWeekFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        binding = FragmentDashboardWeekBinding.inflate(inflater, container ,false);
+        binding = FragmentDashboardWeekBinding.inflate(inflater, container, false);
         return binding.getRoot();
     }
 
@@ -47,12 +57,49 @@ public class DashboardWeekFragment extends Fragment {
     }
 
     private void initCalendar() {
+        Log.d("bibibla", "initCalendar: calling");
+        //TODO temp event
+        List<WeekViewEvent> events = new ArrayList<>();
+        Calendar startTime = Calendar.getInstance();
+        Calendar endTime = (Calendar) startTime.clone();
+        endTime.add(Calendar.HOUR, 3);
+        WeekViewEvent event1 = new WeekViewEvent(1, "test event 1", startTime, endTime);
+        WeekViewEvent event2 = new WeekViewEvent(2, "test event 2", startTime, endTime);
+        event1.setColor(Color.GRAY);
+        event2.setColor(Color.BLUE);
+        events.add(event1);
+        events.add(event2);
+        //TODO -- end temp
         binding.weekView.setMonthChangeListener(new MonthLoader.MonthChangeListener() {
             @Override
             public List<? extends WeekViewEvent> onMonthChange(int newYear, int newMonth) {
-                List<WeekViewEvent> events = new ArrayList<>();
-                return events;
+                // Return only the events that matches newYear and newMonth.
+                List<WeekViewEvent> matchedEvents = new ArrayList<WeekViewEvent>();
+                for (WeekViewEvent event : events) {
+                    if (eventMatches(event, newYear, newMonth)) {
+                        matchedEvents.add(event);
+                    }
+                }
+                return matchedEvents;
             }
         });
+
+        binding.weekView.setOnEventClickListener(new WeekView.EventClickListener() {
+            @Override
+            public void onEventClick(WeekViewEvent event, RectF eventRect) {
+                Toast.makeText(getContext(), event.getName(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    /**
+     * Checks if an event falls into a specific year and month.
+     * @param event The event to check for.
+     * @param year The year.
+     * @param month The month.
+     * @return True if the event matches the year and month.
+     */
+    private boolean eventMatches(WeekViewEvent event, int year, int month) {
+        return (event.getStartTime().get(Calendar.YEAR) == year && event.getStartTime().get(Calendar.MONTH) == month-1) || (event.getEndTime().get(Calendar.YEAR) == year && event.getEndTime().get(Calendar.MONTH) == month - 1);
     }
 }
