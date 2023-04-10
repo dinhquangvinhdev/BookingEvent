adfcc3ed878779efdb35354bd445fdca77d33aa5
dinhquangvinhdev <121219528+dinhquangvinhdev@users.noreply.github.com>
2023-04-10 13:11:52 +0700

    UI add event

---
+++ app/src/main/java/com/vdev/bookingevent/view/fragment/AddEventFragment.java
@@ -0,0 +1,47 @@
+package com.vdev.bookingevent.view.fragment;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.vdev.bookingevent.R;
+import com.vdev.bookingevent.databinding.FragmentAddEventBinding;
+
+public class AddEventFragment extends Fragment {
+
+    private FragmentAddEventBinding binding;
+
+    public AddEventFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding = FragmentAddEventBinding.inflate(inflater , container , false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
